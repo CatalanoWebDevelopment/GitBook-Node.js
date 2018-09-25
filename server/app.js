fc7e19f5b80d2cd6be815c0744bbc560f53b783f
catalanowebdevelopment <catalano.work@gmail.com>
2018-09-25 10:38:10 -0400

    Started working on PieAPI

--- server/app.js
+++ server/app.js
@@ -3,5 +3,8 @@ var app = express();
 var port = 3000
 var test = require('./controllers/testcontroller');
 
+var sequelize = require('./db');
+sequelize.sync();
+
 app.listen(port);
 app.use('/test', test);
