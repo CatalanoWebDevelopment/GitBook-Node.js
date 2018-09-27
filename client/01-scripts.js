dde2155bac90270ccc149c857fd15f43ebdfe1e5
catalanowebdevelopment <catalano.work@gmail.com>
2018-09-27 19:37:10 -0400

    Added functions

--- client/01-scripts.js
+++ client/01-scripts.js
@@ -1,13 +1,13 @@
 function fetchHelloDataFromAPI() {
-    fetch('http://localhost:3000/test/helloclient', { //1
+    fetch('http://localhost:3000/test/helloclient', { 
         method: 'GET', 
-        headers: new Headers({ //2
+        headers: new Headers({ 
           'Content-Type': 'application/json'
         })
     })
         .then(function (response) {
             console.log("Fetch response:", response)
-            return response.text(); //3
+            return response.text(); 
         })
         .then(function (text) {
             console.log(text);
@@ -18,20 +18,89 @@ function postToOne(){
     let url = 'http://localhost:3000/test/one';
 
     fetch(url, {
-      method: 'POST',             //1
+      method: 'POST',             
       headers: new Headers({
         'Content-Type': 'application/json'
       })
     }).then(
-        function(response){   //2
+        function(response){   
             return response.text()
         })
     .catch(
-        function(error){   //3
+        function(error){   
             console.error('Error:', error)
         })
     .then(
-        function(response){   //4
+        function(response){   
             console.log('Success:', response);
+        })
+}
+
+function postToOneArrow(){
+    let url = 'http://localhost:3000/test/one';
+
+    fetch(url, {  
+      method: 'POST', 
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(res => res.text()) 
+    .catch(error => console.error('Error:', error)) 
+    .then(response => console.log('Success:', response)); 
+}
+
+function postData() {
+    
+    let content = { testdata: { item: 'This was saved!' } };
+
+    
+    let testDataAfterFetch = document.getElementById('test-data');
+    let createdAtAfterFetch = document.getElementById('created-at');
+
+    fetch('http://localhost:3000/test/seven', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(content)  
+    })
+    .then(response => response.json())
+    .then(function (text) {
+        console.log(text);
+        
+        testDataAfterFetch.innerHTML = text.testdata.testdata; 
+        createdAtAfterFetch.innerHTML = text.testdata.createdAt;
+    });
+}
+
+function fetchFromOneDisplayData(){
+   
+    let url = 'http://localhost:3000/test/one';
+    let dataView = document.getElementById('display-one');   
+
+   
+    fetch(url, {
+      method: 'GET', 
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(
+        function(response){
+            return response.json()
+        })
+    .catch(
+        function(error){
+            console.error('Error:', error)
+        })
+    .then(
+        function(results){
+            let myList = document.querySelector('#getjson');
+
+            for (r of results){ 
+                console.log('Response:', r.testdata);
+                var listItem = document.createElement('li');  
+                listItem.innerHTML = r.testdata;
+                myList.appendChild(listItem);
+            }
         })
 }
