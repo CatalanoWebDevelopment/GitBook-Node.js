7365a6d55884ffc41a569df3988efc5353f7043e
catalanowebdevelopment <catalano.work@gmail.com>
2018-09-29 13:51:45 -0400

    updates

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var app = express();
 var port = 3000
 var test = require('./controllers/testcontroller');
 var user = require('./controllers/usercontroller');
+var authTest = require('./controllers/authtestcontroller');
 var sequelize = require('./db');
 var bodyParser = require('body-parser');
 
@@ -14,5 +15,8 @@ app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
 app.use('/test', test);
 app.use('/api/user', user);
+app.use(require('./middleware/validate-session'))
+app.use('/authtest', authTest);
+
 
 app.listen(port);
